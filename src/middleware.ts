c46b31e457f92177a89f86d84881bcfa21367d6d
goncang1512 <samuderanst@gmail.com>
2024-08-31 21:07:01 +0700

    new version book hub with new fitur

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 import withAuth from "./lib/middleware/withAuth";
-// Middleware
+
 export const mainmiddleware = async () => {
   const res = NextResponse.next();
   return res;
